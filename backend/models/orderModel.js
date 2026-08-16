669aee0962633ff55e9aff18299939231dfd6052
Sarthakkhade07 <2023chb1071@iitrpr.ac.in>
2026-08-16 23:25:42 +0530

    first commit

---
+++ backend/models/orderModel.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose'
+
+const orderSchema = new mongoose.Schema({
+    userId : {
+        type: String,
+        required: true
+    },
+    items : {
+        type: Array,
+        required: true
+    },
+    amount : {
+        type: Number,
+        required: true
+    },
+    address : {
+        type: Object,
+        required: true
+    },
+    status : {
+        type: String,
+        required: true,
+        default:'Order Placed'
+    },
+    payment : {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    date : {
+        type: Number,
+        required: true,
+    },
+    paymentMethod: {
+    type: String,
+    required: true
+    }
+})
+
+
+const orderModel = mongoose.models.orderModel || mongoose.model('order', orderSchema)
+
+export default orderModel;
---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Title from '../components/Title'
+import NewsLetterBox from '../components/NewsLetterBox'
+
+const About = () => {
+  return (
+    <div>
+      <div className=' text-2xl text-center pt-8 border-t'>
+        <Title text1={'ABOUT'} text2={'US'} />
+      </div>
+
+      <div className=' my-10 flex flex-col md:flex-row gap-16'>
+        <img className=' w-full md:max-w-112.5' src={AuthenticatorAssertionResponse.about_img} alt="" />
+        <div className=' flex flex-col justify-center gap-6 md:w-2/4'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, voluptate. Perspiciatis minima dolorem fugiat cum expedita consequuntur quos error, saepe molestiae tempore magnam.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel praesentium iste, reprehenderit quos numquam dolores aut nesciunt!</p>
+            <b className=' text-gray-800'>Our Mission</b>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus doloremque, quidem cupiditate, unde itaque consectetur eius et voluptatem atque alias, non veniam nemo.</p>
+        </div>
+      </div>
+      <div className=' text-4xl py-4 '>
+        <Title text1={'WHY'} text2={'CHOOSE US'} />
+      </div>
+      <div className=' flex flex-col md:flex-row text-sm mb-20'>
+       
+        <div className=' border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assurance</b>
+          <p className=' text-gray-600'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque cupiditate nihil facilis corrupti accusantium, fugit laborum, rerum repellendus quibusdam voluptate nemo minus ad!</p>
+        </div>
+        
+        <div className=' border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Conveniience</b>
+          <p className=' text-gray-600'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque cupiditate nihil facilis corrupti accusantium, fugit laborum, rerum repellendus quibusdam voluptate nemo minus ad!</p>
+        </div>
+
+        <div className=' border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Exceptional Customer Service</b>
+          <p className=' text-gray-600'>Lorem, Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut, modi. ipsum dolor sit amet consectetur adipisicing elit. Neque cupiditate nihil facilis corrupti accusantium, fugit laborum, rerum repellendus quibusdam voluptate nemo minus ad!</p>
+        </div>
+
+      </div>
+      <NewsLetterBox />
+
+    </div>
+  )
+}
+
+export default About
